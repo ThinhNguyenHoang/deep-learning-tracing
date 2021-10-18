5dcdec8a3f00febd15a1f990ad37855e3f7c474c
DESKTOP-TE85INC\admin <none>
2021-10-19 00:43:52 +0700

    ====================== Thịnh Nguyễn Hoàng =============================== + Preprocessing the bottle and zipper classes (Rename and aggregating their images ==> Easier to write custom dataset class)

---
+++ zipper/aggregated_folder/rename.py
@@ -0,0 +1,16 @@
+import os 
+path = './'
+files = os.listdir(path) 
+
+print(files)
+
+for index,file in enumerate(files):
+    if file[-2:] != "py" and "good" in file:
+        print("Not python file") 
+        classname = "zipper_good-";
+        num = file[5:8]
+        print(index, file)
+        print(os.path.join(path, ''.join([classname,num, '.png'])))
+        os.rename(os.path.join(path,file), os.path.join(path, ''.join([classname,num, '.png']))) 
+    else:
+        print("IS a python file") 
